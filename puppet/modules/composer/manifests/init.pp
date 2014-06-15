69ab7b5461bc708c5d1a0f2f234ac3c5800a796a
alenoosh <hope2end@gmail.com>
2014-06-15 11:14:44 +0430

    Contents of Vagrant repo is moved to this repo.

---
+++ puppet/modules/composer/manifests/init.pp
@@ -0,0 +1,23 @@
+class composer
+{
+
+	exec { 'install composer':
+		command => 'curl -s https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin',
+		require => Package['php5-cli'],
+		unless => "[ -f /usr/local/bin/composer ]"
+	}
+
+	exec { 'global composer':
+		command => "sudo mv /usr/local/bin/composer.phar /usr/local/bin/composer",
+		require => Exec['install composer'],
+		unless => "[ -f /usr/local/bin/composer ]"
+	}
+
+	exec { 'install php check scripts':
+        command => "/bin/sh -c 'cd /var/www/ && composer install'",
+		require => [Exec['global composer']],
+        onlyif => [ "test -f /var/www/composer.json" ],
+        creates => "/var/www/vendor/autoload.php",
+        timeout => 900,
+	}
+}
